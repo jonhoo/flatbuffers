5f08e3786eac392dc4b88c36b5a3e56c20e5b20a
Jon Gjengset <jon.gjengset@helsing.ai>
2024-08-21 12:46:47 +0200

    Apply onnxruntime flatbuffers patch
    https://github.com/microsoft/onnxruntime/blob/v1.17.1/cmake/patches/flatbuffers/flatbuffers.patch

--- src/idl_gen_rust.cpp
+++ src/idl_gen_rust.cpp
@@ -406,7 +406,8 @@ class RustGenerator : public BaseGenerator {
     // example: f(A, D::E)          -> super::D::E
     // does not include leaf object (typically a struct type).
 
-    size_t i = 0;
+    // fix unused but set variable warning
+    //size_t i = 0;
     std::stringstream stream;
 
     auto s = src->components.begin();
@@ -417,7 +418,7 @@ class RustGenerator : public BaseGenerator {
       if (*s != *d) { break; }
       ++s;
       ++d;
-      ++i;
+      //++i;
     }
 
     for (; s != src->components.end(); ++s) { stream << "super::"; }
